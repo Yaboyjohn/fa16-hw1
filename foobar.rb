08f90826c069ad5a93ac22c2f7b7283a71c24411
John Ang <johnang@berkeley.edu>
2016-09-17 21:20:03 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,18 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! do |elem|
+      num = elem.to_i
+      num += 2
+
+      #  new_array.push(num)
+    end
+    sum = 0
+    a.each do |elem|
+      if elem % 2 == 0 && elem < 10
+        sum += elem
+      end
+    end
+    return sum
   end
 end
-
-
